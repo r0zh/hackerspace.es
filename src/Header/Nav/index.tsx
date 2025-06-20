88211be36437bf2b7e9983da2e3ca486b141ec81
r0zh <r0zh017@proton.me>
2025-06-20 02:11:47 +0000

    feat: chage header and footer icons

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -16,10 +16,11 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
 			{navItems.map(({ link }, i) => {
 				return <CMSLink key={i} {...link} appearance="link" />;
 			})}
-			<Link href="/search">
+			{/* Commenting out the search icon for now, as we dont have any posts */}
+			{/* <Link href="/search">
 				<span className="sr-only">Search</span>
 				<SearchIcon className="w-5 text-primary" />
-			</Link>
+			</Link> */}
 		</nav>
 	);
 };
